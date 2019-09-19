a61db6b8018e81336d3776437bbed23ef3410732
lfrei@raptus.com <lfrei@raptus.com>
2019-09-19 12:58:04 +0200

    added text and telegraf fix

--- std-linux/bootstrap.sh
+++ std-linux/bootstrap.sh
@@ -8,13 +8,17 @@ OLDDIR="`pwd`"
 cd $STD_PATH
 
 echo "###"
-echo "###"
-echo "###"
-echo "###"
 echo
-echo "### Bootstrapping this system with Butterlan standards"
+echo '______ _   _ _____ _____ ___________ _       ___   _   _        _     _____ _   _ _   ___   __'
+echo '| ___ \ | | |_   _|_   _|  ___| ___ \ |     / _ \ | \ | |      | |   |_   _| \ | | | | \ \ / /'
+echo '| |_/ / | | | | |   | | | |__ | |_/ / |    / /_\ \|  \| |______| |     | | |  \| | | | |\ V / '
+echo '| ___ \ | | | | |   | | |  __||    /| |    |  _  || . ` |______| |     | | | . ` | | | |/   \ '
+echo '| |_/ / |_| | | |   | | | |___| |\ \| |____| | | || |\  |      | |_____| |_| |\  | |_| / /^\ \'
+echo '\____/ \___/  \_/   \_/ \____/\_| \_\_____/\_| |_/\_| \_/      \_____/\___/\_| \_/\___/\/   \/'
+echo
+echo
+echo "### Bootstrapping this system with BUTTERLAN standards"
 echo "    Logfile: $LOGFILE"
-#askConfirmation
 
 source $STD_PATH/bootstrap-fedora.sh
 
