5a182b64eae2aebd12b12d158b3c1408366ee77a
lfrei@raptus.com <lfrei@raptus.com>
2019-10-10 21:15:33 +0200

    more work on linuxgsm

--- std-linuxgsm/lanparty-linuxgsm.sh
+++ std-linuxgsm/lanparty-linuxgsm.sh
@@ -12,50 +12,22 @@ cd $GSM_PATH
 chmod +x $GSM_PATH/scripts/*
 
 #configure gameserver
-GAMESERVER=
+GAMESERVER=""
 
 echo "###"
 echo "###"
 echo "###"
 echo "###"
 echo
-
-echo "### Standard setup for linuxgsm cache"
+echo "### Standard setup for LinuxGSM"
 echo "    Logfile: $LOGFILE"
-
-echo "### Chose your Gameserver"
-TEMP_PATH=$(mktemp -d)
-cd $TEMP_PATH
-wget -O linuxgsm.sh https://linuxgsm.sh >> $LOGFILE 2>&1
-chmod +x linuxgsm.sh
-./linuxgsm.sh list
-
-
-
-if [ -d "/data/storage1" ]
-then
-    mkdir -m 755 -p /data/storage1/cache
-else
-    echo "Cannot find cache folder /data/storage1. This setup is made for two identical disk storages"
-    exit 1
-fi
-if [ -d "/data/storage1" ]
-then
-    mkdir -m 755 -p /data/storage2/cache
-else
-    echo "Cannot find cache folder /data/storage2. This setup is made for two identical disk storages"
-    exit 1
-fi
 
 # ATTENTION: Dependencies!!!
 source $GSM_PATH/parts-fedora/standard-linuxgsm-components.inc
-source $GSM_PATH/parts-fedora/standard-gameserver.inc
+source $GSM_PATH/parts-fedora/standard-gameserver-environment
 
 ###
 echo "### Finishing installation"
-#echo "alias logtail=\"cd /var/log ; tail -f cron messages secure /data/*/logs/*.log\"" >> /root/.bashrc_local
-#echo ""
-#echo ""
 
 markAsDone
 
---
+++ std-linuxgsm/scripts/setup-gameserver.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source /root/scripts/blan/common.inc
+cd $GSM_PATH
+
+#configure gameserver
+GAMESERVER=""
+
+# ATTENTION: Dependencies!!!
+source $GSM_PATH/parts-fedora/standard-gameserver-environment
+
+#eof
