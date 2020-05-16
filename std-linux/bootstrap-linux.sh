9c02530a4a6c3e064240994e494174264e1341d5
lfrei@raptus.com <lfrei@raptus.com>
2020-05-16 15:31:48 +0200

    package changes an optimization for centos8

--- std-linux/bootstrap-linux.sh
+++ std-linux/bootstrap-linux.sh
@@ -5,14 +5,15 @@ dnf -y clean all >> $LOGFILE 2>&1
 
 echo "### Installing base packages"
 dnf -y upgrade >> $LOGFILE 2>&1
+dnf -y install epel-release
 dnf -y update ca-certificates >> $LOGFILE 2>&1
-dnf -y install acpid apg bzip2 gzip unzip htop nano nmap nload net-tools rsync \
+dnf -y install acpid bzip2 gzip unzip nano nmap net-tools rsync \
                tcpdump telnet traceroute tree unzip uuid pciutils nvme-cli \
-               wget curl whois readline-devel openssl-devel bash-completion \
-               sshfs sysstat dbus vim iperf iftop screen symlinks >> $LOGFILE 2>&1
+               wget curl readline-devel openssl-devel bash-completion \
+               sysstat dbus vim iperf3 iftop screen symlinks >> $LOGFILE 2>&1
 
 echo "### Setting up environment"
-cp $STD_PATH/configs-fedora/.bashrc /root/
+rm -f /root/.bashrc && cp $STD_PATH/configs-fedora/.bashrc /root/
 cp $STD_PATH/configs-fedora/.screenrc /root/
 cp $STD_PATH/configs-fedora/.vimrc /root/
 touch /root/.bashrc_local
@@ -32,13 +33,9 @@ echo "### Setting up services"
 chmod +x /etc/rc.local
 systemctl enable acpid.service >> $LOGFILE 2>&1
 systemctl disable kdump.service >> $LOGFILE 2>&1
-systemctl disable nfs-client.target >> $LOGFILE 2>&1
-systemctl disable nfs-config.service >> $LOGFILE 2>&1
-systemctl disable gssproxy.service >> $LOGFILE 2>&1
 
 dnf -y remove polkit polkit-pkla-compat >> $LOGFILE 2>&1
 chmod o=+r /usr/lib/systemd/system/auditd.service
-chmod -x /usr/lib/systemd/system/wpa_supplicant.service
 
 ###
 echo "### Finishing installation"
