0d2ee18e9d64d36ae8399827aff0a9129c5a2a21
apefrei <70510185+apefrei@users.noreply.github.com>
2022-04-11 15:16:43 +0200

    Update bootstrap-linux.sh

--- std-linux/bootstrap-linux.sh
+++ std-linux/bootstrap-linux.sh
@@ -9,7 +9,7 @@ dnf -y install epel-release >> $LOGFILE 2>&1
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org >> $LOGFILE 2>&1
 dnf -y install https://www.elrepo.org/elrepo-release-8.el8.elrepo.noarch.rpm >> $LOGFILE 2>&1
 dnf -y update ca-certificates >> $LOGFILE 2>&1
-dnf -y install acpid bzip2 gzip unzip nano nmap net-tools rsync \
+dnf -y install acpid bzip2 gzip unzip nano nmap net-tools rsync dmidecode \
                tcpdump telnet traceroute tree unzip uuid pciutils nvme-cli \
                wget curl readline-devel openssl-devel bash-completion \
                sysstat dbus vim iperf3 iftop screen symlinks tar dnf-utils >> $LOGFILE 2>&1
