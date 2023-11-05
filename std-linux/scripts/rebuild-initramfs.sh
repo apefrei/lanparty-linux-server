ebe402127481abaf7208aa2b008cd821ed5a2e57
apefrei <70510185+apefrei@users.noreply.github.com>
2023-11-05 12:00:13 +0100

    Repo has moved

--- std-linux/scripts/rebuild-initramfs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-dracut -v -f
-# eof
--- std-linux/scripts/update-grub.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Script to run after /etc/default/grub has been modified
-#
-
-cp -a /boot/grub2/grub.cfg{,.orig}
-grub2-mkconfig -o /boot/grub2/grub.cfg
-
-# eof
